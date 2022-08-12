453b2302f4414c35077390b589c9e672b11d79b8
Cezar Cardoso <cezar.deev@gmail.com>
2022-08-12 15:00:08 -0300

    Atualizar

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
-    $(window).scroll(function(){
+    //--Evento ao rolar o scroll do mouse--//
+    $(window).scroll(function(){  
+        //--Barra Superior verde--//
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scrollY > 500){
+        //--Seta de Subir a tela--//
+        if(this.scrollY > 500){ 
             $('.scroll-up-btn').addClass("show");
         }else{
-            $('.scroll-up-btn').removeClass("show");
+            $('.scroll-up-btn').removeClass("show"); 
         }
     });
      $('.scroll-up-btn').click(function(){
