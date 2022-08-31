db977f7e61aeeacf67df02a4445369b765a15ae5
Cezar Cardoso <cezar.deev@gmail.com>
2022-08-31 14:23:29 -0300

    Atualizar

--- Articles/EngenhariaSoftware/001-Article/001-Articles.script.js
+++ Articles/EngenhariaSoftware/001-Article/001-Articles.script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    
     //--Evento ao rolar o scroll do mouse--//
     $(window).scroll(function(){  
         //--Barra Superior verde--//
@@ -14,46 +15,10 @@ $(document).ready(function(){
             $('.scroll-up-btn').removeClass("show"); 
         }
     });
-     $('.scroll-up-btn').click(function(){
+
+    $('.scroll-up-btn').click(function(){
          $('html').animate({scrollTop: 0});
 
-     });
-    //--Escritas Automaticas--//
-     var typed = new Typed(".typing", {
-         strings:[ "Supervisor de Sistemas", "Engenheiro de Software", "Desenvolvedor",],
-         typeSpeed:100,
-         backSpeed:60,
-         loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:[ "Supervisor de Sistemas", "Engenheiro de Software", "Desenvolvedor",],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
     });
-
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-    $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:false
-            },
-            600:{
-                items:2,
-                nav:false
-            },
-            1000:{
-                items:3,
-                nav:false
-            }
-        }
-    });
+    
 });
