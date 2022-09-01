081997d0d25248f6af83cd07db2a5ac7771dc796
Cezar Cardoso <cezar.deev@gmail.com>
2022-09-01 16:24:47 -0300

    ok

---
+++ Articles/FrontEnd/Articles.FrontEnd/001-Article.FrontEnd/001-Articles.BackEnd.script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    //--Evento ao rolar o scroll do mouse--//
+    $(window).scroll(function(){  
+        //--Barra Superior verde--//
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        //--Seta de Subir a tela--//
+        if(this.scrollY > 500){ 
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show"); 
+        }
+    });
+
+    $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+    });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    
+});
